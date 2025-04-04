2f1c917925841e66b1596756a74df5fba7bfc177
Troy <tprebenda@gmail.com>
2025-04-04 15:26:10 -0400

    add react big calendar + prisma schema v1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,53 @@
-import Image from "next/image";
+"use client";
+
+import { useState } from "react";
+import { Calendar, momentLocalizer } from "react-big-calendar";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import moment from "moment";
 import styles from "./page.module.css";
 
-export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+// Setup the localizer by providing the moment Object to the correct localizer.
+const localizer = momentLocalizer(moment);
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+const TEST_EVENTS = [
+  {
+    id: 0,
+    title: "Board meeting",
+    start: new Date(2025, 3, 29, 9, 0, 0),
+    end: new Date(2025, 3, 29, 13, 0, 0),
+  },
+  {
+    id: 1,
+    title: "MS training",
+    allDay: true,
+    start: new Date(2025, 3, 29, 13, 0, 0),
+    end: new Date(2025, 3, 29, 16, 30, 0),
+  },
+  {
+    id: 2,
+    title: "Team lead meeting",
+    start: new Date(2025, 3, 29, 8, 30, 0),
+    end: new Date(2025, 3, 29, 12, 30, 0),
+  },
+  {
+    id: 11,
+    title: "Birthday Party",
+    start: new Date(2025, 3, 30, 7, 0, 0),
+    end: new Date(2025, 3, 30, 10, 30, 0),
+  },
+];
+
+export default function MyCalendar() {
+  const events = useState([]);
+  return (
+    <div className={styles.container}>
+      <Calendar
+        localizer={localizer}
+        events={TEST_EVENTS}
+        startAccessor="start"
+        endAccessor="end"
+        defaultView="month"
+      />
     </div>
   );
 }
