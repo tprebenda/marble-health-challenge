ecc6f067fa8d9ea4012d53dcf3ea76385ffe9bb9
Troy <tprebenda@gmail.com>
2025-04-06 18:34:46 -0400

    add date picker validation

--- src/components/MeetingDialog.tsx
+++ src/components/MeetingDialog.tsx
@@ -1,15 +1,18 @@
+import { useState } from "react";
+import moment from "moment";
+
 import DialogTitle from "@mui/material/DialogTitle";
 import Dialog from "@mui/material/Dialog";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 import DialogContent from "@mui/material/DialogContent";
 import DialogActions from "@mui/material/DialogActions";
-import Paper, { PaperProps } from "@mui/material/Paper";
 import Button from "@mui/material/Button";
-import { DateTimePicker } from "@mui/x-date-pickers/DateTimePicker";
-import { styled } from "@mui/material/styles";
-import Grid from "@mui/material/Grid";
 import DraggablePaper from "./DraggablePaper";
+import Paper from "@mui/material/Paper";
+import { styled } from "@mui/material/styles";
+import { DateTimePicker } from "@mui/x-date-pickers/DateTimePicker";
+import Grid from "@mui/material/Grid";
 
 // Styled Paper element to hold Date Pickers in MUI grid component
 const PickerGridItem = styled(Paper)(({ theme }) => ({
@@ -28,6 +31,37 @@ interface MeetingDialogProps {
 }
 
 function MeetingDialog({ handleClose, open }: MeetingDialogProps) {
+  const [startDate, setStartDate] = useState<moment.Moment | null>(null);
+  const [endDate, setEndDate] = useState<moment.Moment | null>(null);
+  const [error, setError] = useState<string | null>(null);
+
+  const onDialogClose = () => {
+    setStartDate(null);
+    setEndDate(null);
+    setError(null);
+    handleClose();
+  };
+
+  const onStartDateChange = (newValue: moment.Moment | null) => {
+    setStartDate(newValue);
+    if (endDate && newValue && newValue.isAfter(endDate)) {
+      // Disable submit on invalid date range
+      setError("Start date must be before end date");
+    } else {
+      setError(null);
+    }
+  };
+
+  const onEndDateChange = (newValue: moment.Moment | null) => {
+    setEndDate(newValue);
+    if (startDate && newValue && newValue.isBefore(startDate)) {
+      // Disable submit on invalid date range
+      setError("End date must be after start date");
+    } else {
+      setError(null);
+    }
+  };
+
   return (
     <Dialog
       onClose={handleClose}
@@ -39,11 +73,11 @@ function MeetingDialog({ handleClose, open }: MeetingDialogProps) {
           onSubmit: (event: React.FormEvent<HTMLFormElement>) => {
             event.preventDefault();
             // TODO: CALL FUNCTION TO FETCH FROM DB (PASS FUNCTION AS PROPS?)
-            // const formData = new FormData(event.currentTarget);
-            // const formJson = Object.fromEntries((formData as any).entries());
-            // const email = formJson.email;
-            // console.log(email);
-            handleClose();
+            const formData = new FormData(event.currentTarget);
+            console.log(formData);
+            const formJson = Object.fromEntries((formData as any).entries());
+            console.log(formJson);
+            onDialogClose();
           },
         },
       }}
@@ -77,20 +111,44 @@ function MeetingDialog({ handleClose, open }: MeetingDialogProps) {
           <Grid container spacing={2} mt={2}>
             <Grid size={6}>
               <PickerGridItem>
-                <DateTimePicker label="Start Date" />
+                <DateTimePicker
+                  label="Start Date"
+                  value={startDate}
+                  onChange={onStartDateChange}
+                  slotProps={{
+                    textField: {
+                      error: !!error,
+                      helperText: error,
+                      required: true,
+                    },
+                  }}
+                />
               </PickerGridItem>
             </Grid>
             <Grid size={6}>
               <PickerGridItem>
-                <DateTimePicker label="Start Date" />
+                <DateTimePicker
+                  label="End Date"
+                  value={endDate}
+                  onChange={onEndDateChange}
+                  slotProps={{
+                    textField: {
+                      error: !!error,
+                      helperText: error,
+                      required: true,
+                    },
+                  }}
+                />
               </PickerGridItem>
             </Grid>
           </Grid>
         </Box>
       </DialogContent>
       <DialogActions>
-        <Button onClick={handleClose}>Cancel</Button>
-        <Button type="submit">Add Event</Button>
+        <Button onClick={onDialogClose}>Cancel</Button>
+        <Button type="submit" disabled={!!error}>
+          Add Event
+        </Button>
       </DialogActions>
     </Dialog>
   );
